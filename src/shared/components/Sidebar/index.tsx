d0a9a8e0439122a9c27d2205daf5da58a27d6195
jovijovi_john <jvc.goncalves@discente.ufma.br>
2026-03-28 18:17:42 -0300

    fix: padding sidebar

--- src/shared/components/Sidebar/index.tsx
+++ src/shared/components/Sidebar/index.tsx
@@ -4,8 +4,8 @@ import SidebarIcons from "../SidebarIcons";
 
 function Sidebar() {
   return (
-    <aside className="fixed bg-white inset-y-0 left-0 z-40 flex h-dvh w-[min(100vw,300px)] shrink-0 flex-col overflow-hidden border-r border-border ">
-      <header className="shrink-0 border-b border-border px-5 pb-8 pt-8 mb-8">
+    <aside className="fixed bg-white inset-y-0 left-0 z-40 flex h-dvh w-[min(100vw,300px)] shrink-0 flex-col overflow-hidden border-r border-border py-4">
+      <header className="shrink-0 border-b border-border px-5 pb-8 pt-4 mb-8">
         <BrandLogo />
       </header>
       <SidebarIcons />
